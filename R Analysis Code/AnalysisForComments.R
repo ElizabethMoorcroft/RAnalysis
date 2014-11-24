3f9b8d7d6cdebc34b73aae98f33d4ba889a77367
ElizabethMoorcroft <e.moorcroft@ucl.ac.uk>
2014-11-24 14:54:09 +0000

    Code for analysis comments - camera error

--- R Analysis Code/AnalysisForComments.R
+++ R Analysis Code/AnalysisForComments.R
@@ -217,10 +217,10 @@ calculate.plot<-function(profilewidth, Data, Speed, Time, NoOfIterations, Densit
 
 ### - Points with CI for each model and percentage error
 setwd(DIR_IMG)
-pdf("AverageModelBias - callerror.pdf")
+pdf("AverageModelBias - cameraerror.pdf")
 
 # Sets the outline of the plot
-plot(type="n",0,0,xlab="Model Number",ylab="",xlim=c(1,25),ylim=c(-15,15),axes=FALSE,main="Call Error")
+plot(type="n",0,0,xlab="Model Number",ylab="",xlim=c(1,25),ylim=c(-15,15),axes=FALSE,main="Camera Error")
 abline(h=0,col="grey",lty=2)
 
 list.of.models<-c(1:25)
@@ -249,9 +249,9 @@ for(Column in 1:dim(Data)[2]){
         error.counter<-1
         for(error in 1:length(percentage.error)){
             # Calculate profile width
-            c.angle<-CallAngleI+CallAngleI*(percentage.error[error]/100)
+            c.angle<-CameraWidthI+CameraWidthI*(percentage.error[error]/100)
             if(c.angle>(2*pi)){c.angle=2*pi} else if(c.angle<0){c.angle=0}
-            profilewidth<-calcProfileWidth(c.angle, CameraWidthI, CameraRadiI )[[1]]
+            profilewidth<-calcProfileWidth(CallAngleI, c.angle, CameraRadiI )[[1]]
             calculate.plot(profilewidth, Data=Data[,Column], Speed, Time, NoOfIterations, Density, modelcount=example.points.both, error.counter)
             error.counter=error.counter+1
         }# error loop
